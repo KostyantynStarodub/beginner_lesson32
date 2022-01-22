b9e04d1bd0d6c2e42179f804e81e99cc23bf7eab
KostyantynStarodub <92105629+KostyantynStarodub@users.noreply.github.com>
2022-01-22 13:45:36 +0200

    beginner_lesson32

---
+++ main.js
@@ -0,0 +1,173 @@
+//begin1
+// let glor = prompt("Введите сторону квадрата")
+// let P = 4 * glor
+// alert(P)
+// console.log(P)
+
+//begin25
+// let x = prompt("Введите значение х")
+// let y = 3 * (x ** 6) - 6 * (x ** 2) - 7
+// alert(y)
+// console.log(y)
+
+//begin2
+// let a = prompt("Введите сторону квадрата")
+// let S = a**2
+// alert(S)
+// console.log(S)
+
+//begin3
+// let a = Number(prompt("Введите первую сторону прямоугольника"))
+// let b = Number(prompt("Введите вторую сторону прямоугольника"))
+// let S = a*b
+// let P = 2 * (a + b)
+// alert("Площадь прямоугольника: " + S)
+// alert("Периметр прямоугольника: " + P)
+// console.log("Площадь прямоугольника: " + S)
+// console.log("Периметр прямоугольника: " + P)
+
+//begin4
+// let d = prompt("Введите диаметр окружности")
+// let L = 3.14 * d
+// alert(L)
+// console.log(L)
+
+//begin5
+// let a = prompt("Введите длину ребра куба")
+// let S = 6 * a ** 2
+// let V = a ** 3
+// alert("Площадь поверхности куба: " + S)
+// alert("Объем куба: " + V)
+// console.log("Площадь поверхности куба: " + S)
+// console.log("Объем куба: " + V)
+
+//begin6
+// let a = Number(prompt("Введите длину первого ребра паралелепипеда"))
+// let b = Number(prompt("Введите длину второго ребра паралелепипеда"))
+// let c = Number(prompt("Введите длину третьего ребра паралелепипеда"))
+// let V = a * b * c
+// let S = 2 * (a * b + b * c + a * c)
+// alert("Объем паралелепипеда: " + V)
+// alert("Площадь паралелепипеда: " + S)
+// console.log("Объем паралелепипеда: " + V)
+// console.log("Площадь паралелепипеда: " + S)
+
+//begin7
+// let R = prompt("Введите радиус окружности")
+// let L = 2 * 3.14 * R
+// let S = 3.14 * R ** 2
+// alert("Длина окружности: " + L)
+// alert("Площадь круга: " + S)
+// console.log("Длина окружности: " + L)
+// console.log("Площадь круга: " + S)
+
+//begin8
+// let a = Number(prompt("Введите первое число"))
+// let b = Number(prompt("Введите второе число"))
+// let r = (a + b)/2
+// alert("Среднее арифметическое: " + r)
+// console.log("Среднее арифметическое: " + r)
+
+//begin9
+// let a = Number(prompt("Введите первое неотрицательное число"))
+// let b = Number(prompt("Введите второе неотрицательное число"))
+// let r = (a * b) ** 0.5
+// alert("Среднее геометрическое: " + r)
+// console.log("Среднее геометрическое: " + r)
+
+//begin10
+// let a = Number(prompt("Введите первое ненулевое число"))
+// let c = Number(prompt("Введите второе ненулевое число"))
+// let b = a ** 2 + c ** 2
+// let d = a ** 2 - c ** 2
+// let s = a ** 2 * c ** 2
+// let m = a ** 2 / c ** 2
+// alert("Сумма квадратов: " + b)
+// alert("Разность квадратов: " + d)
+// alert("Произведение квадратов: " + s)
+// alert("Частное квадратов: " + m)
+// console.log("Сумма квадратов: " + b)
+// console.log("Разность квадратов: " + d)
+// console.log("Произведение квадратов: " + s)
+// console.log("Частное квадратов: " + m)
+
+//begin11
+// let a = Number(prompt("Введите первое ненулевое число"))
+// let b = Number(prompt("Введите второе ненулевое число"))
+// let h = Math.abs(a) + Math.abs(b)
+// let g = Math.abs(a) - Math.abs(b)
+// let l = Math.abs(a) * Math.abs(b)
+// let o = Math.abs(a) / Math.abs(b)
+// alert("Сумма модулей: " + h)
+// alert("Разность модулей: " + g)
+// alert("Произведение модулей: " + l)
+// alert("Частное модулей: " + o)
+// console.log("Сумма модулей: " + h)
+// console.log("Разность модулей: " + g)
+// console.log("Произведение модулей: " + l)
+// console.log("Частное модулей: " + o)
+
+//begin12
+// let a = Number(prompt("Введите первый катет"))
+// let b = Number(prompt("Введите второй катет"))
+// let c = (a ** 2 + b ** 2) ** 0.5
+// let P = a + b + c
+// alert("Гипотенуза: " + c)
+// alert("Периметр: " + P)
+// console.log("Гипотенуза: " + c)
+// console.log("Периметр: " + P)
+
+//begin13
+// let a = Number(prompt("Введите первый радиус"))
+// let b = Number(prompt("Введите второй радиус"))
+// let s = 3.14 * a ** 2 
+// let d = 3.14 * b ** 2
+// let g = s - d
+// alert("Площадь первого круга: " + s)
+// alert("Площадь второго круга: " + d)
+// alert("Площадь кольца: " + g)
+// console.log("Площадь первого круга: " + s)
+// console.log("Площадь второго круга: " + d)
+// console.log("Площадь кольца: " + g)
+
+//begin14
+// let L = Number(prompt("Введите длину окружности"))
+// let R = L / (2 * 3.14)
+// let S = 3.14 * R ** 2
+// alert("Радиус окружности: " + R)
+// alert("Площадь круга: " + S)
+// console.log("Радиус окружности: " + R)
+// console.log("Площадь круга: " + S)
+
+//begin15
+// let S = Number(prompt("Введите площадь круга"))
+// let D = 2 * (S / 3.14) ** 0.5
+// let L = 3.14 * D
+// alert("Диаметр круга: " + D)
+// alert("Длина окружности: " + L)
+// console.log("Диаметр круга: " + D)
+// console.log("Длина окружности: " + L)
+
+//begin26
+// let x = Number(prompt("Введите значение х"))
+// let y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2
+// alert(y)
+// console.log(y)
+
+//begin27
+// let A = Number(prompt("Введите число"))
+// let A1 = A ** 2
+// let A2 = A ** 4
+// let A3 = A ** 8
+// alert(A1 + " " + A2 + " " + A3)
+// console.log(A1 + " " + A2 + " " + A3)
+
+//begin28
+// let A = Number(prompt("Введите число"))
+// let A1 = A ** 2
+// let A2 = A ** 3
+// let A3 = A ** 5
+// let A4 = A ** 10
+// let A5 = A ** 15
+// alert(A1 + " " + A2 + " " + A3 + " " + A4 + " " + A5)
+// console.log(A1 + " " + A2 + " " + A3 + " " + A4 + " " + A5)
